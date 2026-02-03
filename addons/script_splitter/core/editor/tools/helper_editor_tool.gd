eb2e06c73d5f3267f72f88fccb2d49f4876d2626
H0m3 <aid.me.service@gmail.com>
2026-02-03 02:52:14 -0300

    Try update #27

--- addons/script_splitter/core/editor/tools/helper_editor_tool.gd
+++ addons/script_splitter/core/editor/tools/helper_editor_tool.gd
@@ -8,6 +8,21 @@ extends "./../../../core/editor/tools/editor_tool.gd"
 #	author:		"Twister"
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
+func _fallback(ctrl : Control, expt : int = 5) -> void:
+	if !is_instance_valid(ctrl) or expt < 1:
+		return
+	
+	if ctrl.size.x < 100.0 or ctrl.size.y < 100.0:
+		ctrl.set_deferred(&"size_flags_vertical", Control.SIZE_EXPAND_FILL)
+		ctrl.set_deferred(&"size_flags_horizontal", Control.SIZE_EXPAND_FILL)
+		
+		var pnode : Node = ctrl.get_parent()
+		if pnode is Control:
+			ctrl.size = pnode.size
+			
+		_fallback.call_deferred(ctrl, expt - 1)
+		
+
 func _build_tool(control : Node) -> MickeyTool:
 	if control is ScriptEditorBase:
 		return null
@@ -18,16 +33,22 @@ func _build_tool(control : Node) -> MickeyTool:
 	for x : Node in control.get_children():
 		if x is RichTextLabel:
 			var canvas : VBoxContainer = VBoxContainer.new()
-			canvas.size_flags_vertical = Control.SIZE_EXPAND_FILL
-			canvas.size_flags_vertical = Control.SIZE_EXPAND_FILL
 		
 			var childs : Array[Node] = control.get_children()
+			
 			for n : Node in childs:
 				control.remove_child(n)
 				canvas.add_child(n)
 					
 			canvas.size = control.size
+			canvas.size_flags_vertical = Control.SIZE_EXPAND_FILL
+			canvas.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+			
 			mickey = MickeyToolRoute.new(control, canvas, canvas)
+			
+			for z : Node in canvas.get_children():
+				if z is RichTextLabel:
+					_fallback(z)
 			break
 	return mickey
 
